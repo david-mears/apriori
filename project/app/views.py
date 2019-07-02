1679273e7906dc1d6945885175e0069ecfcc7cdb
David Mears <kieukmail@gmail.com>
2019-07-02 20:58:55 +0100

    Have task_list lead with the optimal task

--- project/app/views.py
+++ project/app/views.py
@@ -1,4 +1,6 @@
 from django.contrib.auth.decorators import login_required
+from django.db.models.functions import Cast
+from django.db.models import FloatField
 from django.forms import ModelForm
 from django.shortcuts import render
 from django.urls import reverse_lazy
@@ -15,6 +17,21 @@ class TaskList(ListView):
     login_required = True
     model = Task
     fields = '__all__'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        optimal_task_list = [
+            Task.objects.filter(done=False)
+            .annotate(density=(
+                Cast('importance', FloatField())/Cast('estimated_duration', FloatField())
+            ))
+            .order_by('-density')
+            .first()
+        ]
+        if optimal_task_list[0] == None:
+            optimal_task_list.pop()
+        context['optimal_task_list'] = optimal_task_list
+        return context
 
 class TaskDetail(DetailView):
     login_required = True
