b3ae57acd9c388c3e49012a288a0a2a4539b6f36
David Mears <kieukmail@gmail.com>
2019-07-02 19:51:16 +0100

    Change file location

--- project/apriori/settings.py
+++ project/apriori/settings.py
@@ -57,7 +57,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'apriori', 'templates'),
             os.path.join(BASE_DIR, 'app', 'templates'),
         ],
         'APP_DIRS': True,
